928db5cdcf7e2b043abd3dae586196d9ec7570f9
maojude <maojude04@gmail.com>
2025-04-04 19:18:16 +0800

    Do palindromes exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,33 @@
-const palindromes = function () {
+const palindromes = function (string) {
 
+    // turn string into array of characters
+    const stringArray = string.split('');
+
+    // create a new array to hold filtered characters
+    const filteredArray = stringArray.filter((char) => {
+        
+        if(!(char === ' ' || char === '.' || char === ',' || char === '!' 
+            || char === '?' || char === ':' || char === ';' || char === '-' || char === '_'
+            || char === '(' || char === ')' || char === '[' || char === ']' || char === '{' || char === '}'))
+            return true;
+    
+    });
+
+
+    // create a new array to hold lower case characters
+    const lowerCaseArray = filteredArray.map((char) => {
+        if(typeof char === 'number') return char;
+        else return char.toLowerCase();
+    });
+
+   const finalArray = lowerCaseArray.join('');
+    
+    // check if the filtered array is a palindrome
+    // compare the filtered array with its reverse
+    const reversedArray = lowerCaseArray.slice().reverse().join('');
+    
+    return finalArray === reversedArray ? true : false;
+    
 };
 
 // Do not edit below this line
