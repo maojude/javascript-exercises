1037cf1ec8ac48f3c4949823cedc7924336e4eae
maojude <maojude04@gmail.com>
2025-04-04 17:44:58 +0800

    Do exercise 1

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    let output = '';
+    for(let i = string.length; i > 0; i++){
+        output += string.charAt(i -1);
+    }
 
+    return output;
 };
 
 // Do not edit below this line
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...argsToRemove) {
+    const outputArray = [];
+
+    for(value of array){
+        if(!argsToRemove.includes(value)){
+            outputArray.push(value);
+        }
+    }
+
+    return outputArray;
 };
 
 // Do not edit below this line
