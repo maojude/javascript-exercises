0332d7dee4cb65832f82a3203f4fc1dd379ad864
maojude <maojude04@gmail.com>
2025-04-04 18:22:49 +0800

    Do temperature conversion exercise

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,16 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(num) {
+  const celsius = (num - 32) * (5/9);
+  return roundToOneDecimal(celsius);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(num) {
+  const fahrenheit = (num * (9/5)) + 32;
+  return roundToOneDecimal(fahrenheit);
 };
+
+function roundToOneDecimal(num){
+  return Math.round(num * 10) / 10;
+}
 
 // Do not edit below this line
 module.exports = {
