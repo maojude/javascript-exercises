44f8dba72bba0e91a960eba3b170cf621220b83e
maojude <maojude04@gmail.com>
2025-04-04 18:35:07 +0800

    do calculator exercise

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,44 @@
-const add = function() {
-	
+const add = function(num1,num2) {
+  return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  let output = 0;
+  for(num of array){
+    output += num;
+  }
+  return output;
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  if(array.length == 0) return 0;
+  let output = 1;
+  for(num of array){
+    output *= num;
+  }
+  return output;
 };
 
-const power = function() {
-	
+const power = function(num, power) {
+  let output = num;
+  for(let i = 0; i < power-1; i++){
+    output *= num;
+  }
+
+  return output;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	if(num === 0) return 1;
+  let output = 1;
+  for(let i = 1; i <= num; i++){
+    output *= i;
+  }
+  return output;
 };
 
 // Do not edit below this line
