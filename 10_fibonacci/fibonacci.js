a47edb6ea634b35f91e8d38048ac0f180c3448ef
maojude <maojude04@gmail.com>
2025-04-04 22:06:28 +0800

    Do fibonacci exercise using recursion

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,16 @@
-const fibonacci = function() {
+const fibonacci = function(fiboNum) {
+    let fiboNumInt = +fiboNum;
+    if(fiboNumInt < 0) return "OOPS";
+    else if (fiboNumInt === 0) return 0;
+    else {
+        function findFibo(num){
+            if(num <= 1 ) return num;
+            
+            return findFibo(num - 1) + findFibo(num - 2);
+        }
+
+        return findFibo(fiboNumInt);
+    }
 
 };
 
