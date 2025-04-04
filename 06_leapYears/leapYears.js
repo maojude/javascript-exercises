0fadb1f83e008775e5a470a4add5729784bc07e9
maojude <maojude04@gmail.com>
2025-04-04 18:11:06 +0800

    do LeapYears exercise

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,15 @@
-const leapYears = function() {
+const leapYears = function(num) {
+    let isLeapYear = false;
 
+    let divisibleBy4 = num % 4 === 0;
+    let divisibleBy400 = num % 400 === 0;
+    let divisibleBy100 = num % 100 === 0;
+
+    if(divisibleBy4 && (!divisibleBy100 || divisibleBy400)){
+        isLeapYear = true;
+    }
+
+    return isLeapYear;
 };
 
 // Do not edit below this line
