f164223f662061154a25ff66e41026b533fb3629
maojude <maojude04@gmail.com>
2025-04-04 23:08:42 +0800

    Do findTheOldest using reduce array method

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,25 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+
+    const date = new Date();
+    const currentYear = date.getFullYear();
+
+
+    function getAge(person){
+        if(person.yearOfDeath === undefined){
+            return currentYear - person.yearOfBirth;
+            
+        }
+        return person.yearOfDeath - person.yearOfBirth;
+    }
+    
+    return people.reduce((oldest, person) => {
+        const oldestAge = getAge(oldest);
+        const personAge = getAge(person);
+        return personAge > oldestAge ? person : oldest;
+    });
 
 };
 
 // Do not edit below this line
 module.exports = findTheOldest;
+
